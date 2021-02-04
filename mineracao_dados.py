6f9ffac91fd377cf3221d571b4963144b2eedb8e
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2021-02-04 14:27:19 -0300

    web_scraping

--- mineracao_dados.py
+++ mineracao_dados.py
@@ -21,51 +21,41 @@ planilha = wb.worksheets[0]
     #print(links.text)
 
 driver = webdriver.Chrome(executable_path=r"C:\Users\lukss\Desktop\bot_mtc\chromedriver.exe")
-driver.get("https://scholar.google.com.br/scholar?start=50&q=%22Chatbot%22+AND+%22com%C3%A9rcio+eletr%C3%B4nico%22&hl=pt-BR&as_sdt=0,5&as_ylo=2019&as_yhi=2021")
+
 sleep(3)
 
 
-cont = 2
+cont = 0
 x = 1
 y = 1
 
 
 
-for a in driver.find_elements_by_xpath('.//a'):
-    nome = a.get_attribute('text')
-    link = a.get_attribute('href')
 
-    planilha['A{}'.format(x)] = nome
-    planilha['B{}'.format(y)] = link
-    x += 1
-    y += 1
+while(True):
+        
+    driver.get("https://scholar.google.com.br/scholar?start={}&q=%22Chatbot%22+AND+%22com%C3%A9rcio+eletr%C3%B4nico%22&hl=pt-BR&as_sdt=0,5&as_ylo=2019&as_yhi=2021".format(cont))
+    
+    cont += 10
 
-    cont += 1
+    for a in driver.find_elements_by_xpath('.//a'):
+        
+        
+        nome = a.get_attribute('text')
+        link = a.get_attribute('href')
 
-
-    print(nome)
-    print(link)
-
-    wb.save(r"C:\Users\lukss\Desktop\bot_mtc\Links_MTC2.xlsx")
-
-    if nome == "7":
-        driver.find_element_by_link_text("{}".format(nome)).click()
-        driver.get("{}".format(link))
-        for a in driver.find_elements_by_xpath('.//a'):
-            nome = a.get_attribute('text')
-            link = a.get_attribute('href')
-
-            planilha['A{}'.format(x)] = nome
-            planilha['B{}'.format(y)] = link
-            x += 1
-            y += 1
-
-            cont += 1
+        planilha['A{}'.format(x)] = nome
+        planilha['B{}'.format(y)] = link
+        x += 1
+        y += 1
 
 
-            print(nome)
-            print(link)
-            wb.save(r"C:\Users\lukss\Desktop\bot_mtc\Links_MTC2.xlsx")
+
+        print(nome)
+        print(link)
+
+        wb.save(r"C:\Users\lukss\Desktop\bot_mtc\Teste_10.xlsx")
 
 
+        
                                                     
