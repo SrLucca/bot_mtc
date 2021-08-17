63d7d2f519fa2a8fed6cbb00a30e75240319006d
Lucca Torrezilha Soares <68205690+SrLucca@users.noreply.github.com>
2021-08-17 01:55:49 -0300

    correção na criação das planilhas

--- executor.py
+++ executor.py
@@ -10,12 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def codigo(link,navegador):
     
-    wb = Workbook()
-
-
-    planilha = wb.worksheets[0]
     
-    sleep(3)
 
 
     cont = 0
@@ -29,44 +24,50 @@ def codigo(link,navegador):
 
     
     if navegador == 2:
+
+
         options = EdgeOptions()
         options.use_chromium = True
 
         driver = Edge(options = options)
         driver.maximize_window()
         #print(link)
+        driver.get("https://scholar.google.com.br/scholar?start=0&q={}".format(split[1]))
+
+
         root = os.getcwd()
 
-        try:
-            os.mkdir('planilhas')
         
-        except FileExistsError:
-            os.chdir('planilhas')
-
         wait = WebDriverWait(driver, 10)
 
-        driver.get("https://scholar.google.com.br/scholar?start=0&q={}".format(split[1]))
-        
         while(True):
             
-            items = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '.gs_rt a')))
-            for item in items:
+            try:
+                os.mkdir('planilhas')
+            except FileExistsError:
+                os.chdir(f'{root}\\planilhas')
 
-                nome_artigo = item.text
-                link_artigo = item.get_attribute('href')
-                print('Artigo: ',nome_artigo,'\nLink: ', link_artigo,'\n')
-                planilha['A{}'.format(x)] = nome_artigo
-                planilha['B{}'.format(y)] = link_artigo
-            
-            botao = driver.find_element_by_partial_link_text('Mais')
-            botao.click()
-            
+                wb = Workbook()
 
 
-            
-            #x += 1
-            #y += 1
+                planilha = wb.worksheets[0]
 
+                items = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '.gs_rt a')))
+
+                for item in items:
+
+                    nome_artigo = item.text
+                    link_artigo = item.get_attribute('href')
+                    print('Artigo: ',nome_artigo,'\nLink: ', link_artigo,'\n')
+                    planilha['A{}'.format(x)] = nome_artigo
+                    planilha['B{}'.format(y)] = link_artigo
+                
+                botao = driver.find_element_by_partial_link_text('Mais')
+                botao.click()
+                wb.save("planilha.xlsx")
+                
+                x += 1
+                y += 1
 
             
     
