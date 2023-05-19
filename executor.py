df4ac7410c90612365fedceb5bcf29ab9ac99f2b
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2023-05-18 23:13:09 -0300

    atualizações da forma de usar o selenium

--- executor.py
+++ executor.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from openpyxl import Workbook
-from msedge.selenium_tools import EdgeOptions, Edge
 from time import sleep
 import os
 from selenium.webdriver.common.by import By
@@ -13,8 +12,8 @@ def codigo(link,navegador):
 
 
     cont = 0
-    x = 1
-    y = 1
+    x = 0
+    y = 0
 
     split = link.split('q=')
     print(split)
@@ -25,19 +24,21 @@ def codigo(link,navegador):
     if navegador == 2:
 
 
-        options = EdgeOptions()
-        options.use_chromium = True
-
-        driver = Edge(options = options)
+        driver = webdriver.Edge()
         driver.maximize_window()
         #print(link)
-        driver.get("https://scholar.google.com.br/scholar?start=0&q={}".format(split[1]))
+        driver.get(f"https://scholar.google.com.br/scholar?start=0&q={split[1]}")
 
 
         root = os.getcwd()
 
         
         wait = WebDriverWait(driver, 10)
+
+        wb = Workbook()
+
+
+        planilha = wb.worksheets[0]
 
         while(True):
             
@@ -46,27 +47,28 @@ def codigo(link,navegador):
             except FileExistsError:
                 os.chdir(f'{root}\\planilhas')
 
-                wb = Workbook()
-
-
-                planilha = wb.worksheets[0]
-
                 items = wait.until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '.gs_rt a')))
 
                 for item in items:
 
+                    x += 1
+                    y += 1
+
+                    open_planilha = wb.worksheets.append(object)
+
                     nome_artigo = item.text
                     link_artigo = item.get_attribute('href')
                     print('Artigo: ',nome_artigo,'\nLink: ', link_artigo,'\n')
-                    planilha['A{}'.format(x)] = nome_artigo
-                    planilha['B{}'.format(y)] = link_artigo
+                    planilha[f'A{x}'] = nome_artigo
+                    planilha[f'B{y}'] = link_artigo
+                    
+                    
                 
-                botao = driver.find_element_by_partial_link_text('Mais')
+                botao = driver.find_element(by=By.PARTIAL_LINK_TEXT , value=f"Mais")
                 botao.click()
                 wb.save("planilha.xlsx")
                 
-                x += 1
-                y += 1
+                
 
             
     
